78ba0524b851b64994682ca292ffa67fdcf85611
syedmkazmi <syed.kazmi@kmandt.com>
2016-11-23 11:12:40 +0000

    added a bootstrap folder and bootstrap files

--- app_server/controllers/main.js
+++ app_server/controllers/main.js
@@ -1,3 +1,9 @@
 /**
  * Created by syedkazmi on 04/11/2016.
  */
+
+module.exports.index = function (req, res) {
+
+    res.render('index', { title: 'Express' });
+
+}
--- app_server/routes/index.js
+++ app_server/routes/index.js
@@ -1,9 +1,8 @@
 var express = require('express');
 var router = express.Router();
+var ctrl = require('../controllers/main');
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
-});
+router.get('/', ctrl.index);
 
 module.exports = router;
